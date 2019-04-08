d53dfbbe6fba59b64f6a1ad177f15568e33aca44
SergeiMishchenko <sergei.mish26@gmail.com>
2019-04-08 10:01:52 -0400

    intialized values from List_inArraySlots in skeletonHolmes

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -5,38 +5,28 @@
 
 public class List_inArraySlots {
 
-    private int[] elements;     // container for the elements of the list
-    private int filledElements; // the number of elements in this list
-
-    private static final int INITIAL_CAPACITY = 10;
+    // declare fields here
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-        elements = new int[ INITIAL_CAPACITY];
-        // filledElements has been initialized to the desired value, 0
     }
 
 
     /**
       @return the number of elements in this list
      */
-    public int size() {
-        return filledElements;
-    }
+    // public int size() {
+    // }
 
 
      /**
        @return a string representation of this list,
        in [a,b,c,] format
       */
-    public String toString() {
-        String result = "[";
-        for( int elemIndex = 0; elemIndex < filledElements; elemIndex++)
-            result += elements[ elemIndex] + ",";
-        return result + "]";
-    }
+    // public String toString() {
+    // }
 
 
     /**
@@ -44,32 +34,21 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-     public boolean add( int value) {
-         // expand if necessary
-         if( filledElements == elements.length) expand();
-
-         elements[ filledElements] = value;
-         filledElements++;
-         // idiomatic version: elements[ filledElements++] = value;
-        return true;
-}
+     // public boolean add( int value) {
+     // }
 
 
     /**
       Double the capacity of the List_inArraySlots,
       preserving existing data.
      */
-     private void expand() {
-        System.out.println( "expand... (for debugging)");
-           /* S.O.P. rules for debugging:
-              Working methods should be silent. But during
-              development, the programmer must verify that
-              this method is called when that is appropriate.
-              So test using the println(), then comment it out.
-              */
-        int[] bigger = new int[ elements.length * 2];
-        for( int elemIndex = 0; elemIndex < filledElements; elemIndex++)
-            bigger[ elemIndex] = elements[ elemIndex];
-        elements = bigger;
-     }
+     // private void expand() {
+        // System.out.println( "expand... (for debugging)");
+           // /* S.O.P. rules for debugging:
+              // Working methods should be silent. But during
+              // development, the programmer must verify that
+              // this method is called when that is appropriate.
+              // So test using the println(), then comment it out.
+              // */
+     // }
 }
--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-###Project Title: List_inArraySlots
-####Members: Sergei Mishchenko, Kyra Gunluk (partner)
+# implement a list by storing its elements in an array
 
-####Fields: int filledElements, int[] elements 
-####Constructors: creates an empty int array
-####Possible methods: add, addAtHead, size, expand, 
+Design `List_inArraySlots` to implement a list of integers.
+Your implementation required to be able to store any values that 
+an `int` can hold, in consecutive slots of an array of `int`s.
 
-###Remarks on UserOfList class:
-####tests edge and middle casses of methods to ensure they work
+In general, the array will be only partially populated with list elements.
+That is, the array is generally larger than this list. 
+Occasionally, the array can be completely full, but adding
+another element to the list will cause the `List_inArraySlots`
+class to expand the list, as described in the header comments
+for the `expand()` method in the skeleton.
 
-# implement a list by storing data in an encapsulated array
-
-## features to understand
-- Keep **Encapsulated data** `private`.
-- Trace the changes to the heap and stack during
-an invocation of `expand()`.
-
+The requirement "to store any values that an `int` can hold" 
+implies that the implementation can **not** identify the end of the list
+by the values of the integers stored in the list. That design decision
+would require reserving one integer value as a marker value, which would
+keep the user-programmer from storing that value in the list.
+Therefore, the "marker value" technique is banned for our purposes here.
+(In other contexts, marker values are fine design choices.)
