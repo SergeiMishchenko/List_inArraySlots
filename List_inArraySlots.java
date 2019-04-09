e96645871d4846597b17c33b943f3fab538f6120
SergeiMishchenko <sergei.mish26@gmail.com>
2019-04-09 12:27:19 -0400

    hw4D_16

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -5,28 +5,38 @@
 
 public class List_inArraySlots {
 
-    // declare fields here
+    private int[] list;
+	private int elementsFilled;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
+		list = new int[5];
+		elementsFilled = 0;
     }
 
 
     /**
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
+     public int size() {
+		 return elementsFilled;
+     }
 
 
      /**
        @return a string representation of this list,
        in [a,b,c,] format
       */
-    // public String toString() {
-    // }
+     public String toString() {
+		String result = "[";
+        for (int i = 0; i < elementsFilled; i++) {
+            result += list[i] + ",";
+        }
+        return result + "]";
+		 
+    }
 
 
     /**
@@ -34,21 +44,29 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int value) {
-     // }
+      public boolean add( int value) {
+		if(elementsFilled == list.length) //checks if there slots that are open
+            expand();
+        list[elementsFilled++] = value;
+        return true;
+      }
 
 
     /**
       Double the capacity of the List_inArraySlots,
       preserving existing data.
      */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           // /* S.O.P. rules for debugging:
-              // Working methods should be silent. But during
-              // development, the programmer must verify that
-              // this method is called when that is appropriate.
-              // So test using the println(), then comment it out.
-              // */
-     // }
-}
+      private void expand() {
+         System.out.println( "expand... (for debugging)");
+            /* S.O.P. rules for debugging:
+               Working methods should be silent. But during
+               development, the programmer must verify that
+               this method is called when that is appropriate.
+               So test using the println(), then comment it out.
+               */
+	    int[] storage = list; 
+        list = new int[2 * elementsFilled]; 
+        for(int i = 0; i < storage.length; i++)
+           list[i] = storage[i];
+     }
+ }
--- README.md
+++ README.md
@@ -18,3 +18,24 @@ would require reserving one integer value as a marker value, which would
 keep the user-programmer from storing that value in the list.
 Therefore, the "marker value" technique is banned for our purposes here.
 (In other contexts, marker values are fine design choices.)
+###Project Title: List_inArraySlots
+####Members: Sergei Mishchenko, Kyra Gunluk (partner)
+
+####Fields: 
+int filledElements - counts filled elements in array
+int[] list - contains an array of ints
+####Constructors: 
+creates an empty int array and initalizes elementsFIlled variable
+####methods: 
+size() - returns number of filled Elements
+add (int value) - adds value to the end of the list
+expand () - increases size of the list
+addAtHead
+expand 
+
+###Remarks on UserOfList class:
+tests edge and middle casses of methods to ensure they work
+creates an instance of List_inArraySlots.
+# implement a list by storing data in an encapsulated array
+
+## features to understand
