13433ab360861e1c883e5b7f5eef5bf792aa7527
SergeiMishchenko <sergei.mish26@gmail.com>
2019-04-15 20:58:10 -0400

    hw50_16

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,41 +1,41 @@
-/**
+/*
   Implement a list of integer elements, including
   both data and operations.
  */
 
 public class List_inArraySlots {
 
-    private int[] list;
-	private int elementsFilled;
+    private int[] elements;     // container for the elements of the list
+    private int filledElements; // the number of elements in this list
+
+    private static final int INITIAL_CAPACITY = 10;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-		list = new int[5];
-		elementsFilled = 0;
+        elements = new int[ INITIAL_CAPACITY];
+        // filledElements has been initialized to the desired value, 0
     }
 
 
     /**
       @return the number of elements in this list
      */
-     public int size() {
-		 return elementsFilled;
-     }
+    public int size() {
+        return filledElements;
+    }
 
 
      /**
        @return a string representation of this list,
        in [a,b,c,] format
       */
-     public String toString() {
-		String result = "[";
-        for (int i = 0; i < elementsFilled; i++) {
-            result += list[i] + ",";
-        }
+    public String toString() {
+        String result = "[";
+        for( int elemIndex = 0; elemIndex < filledElements; elemIndex++)
+            result += elements[ elemIndex] + ",";
         return result + "]";
-		 
     }
 
 
@@ -44,29 +44,99 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-      public boolean add( int value) {
-		if(elementsFilled == list.length) //checks if there slots that are open
-            expand();
-        list[elementsFilled++] =  value;
+     public boolean add( int value) {
+         // expand if necessary
+         if( filledElements == elements.length) expand();
+
+         elements[ filledElements++] = value;
+         // idiomatic version: elements[ filledElements++] = value;
         return true;
-      }
+}
 
 
     /**
       Double the capacity of the List_inArraySlots,
       preserving existing data.
      */
-      private void expand() {
-         System.out.println( "expand... (for debugging)");
-            /* S.O.P. rules for debugging:
-               Working methods should be silent. But during
-               development, the programmer must verify that
-               this method is called when that is appropriate.
-               So test using the println(), then comment it out.
-               */
-	    int[] storage = list; 
-        list = new int[2 * elementsFilled]; 
-        for(int i = 0; i < storage.length; i++)
-           list[i] = storage[i];
+     private void expand() {
+        System.out.println( "expand... (for debugging)");
+           /* S.O.P. rules for debugging:
+              Working methods should be silent. But during
+              development, the programmer must verify that
+              this method is called when that is appropriate.
+              So test using the println(), then comment it out.
+              */
+        int[] bigger = new int[ elements.length * 2];
+        for( int elemIndex = 0; elemIndex < filledElements; elemIndex++)
+            bigger[ elemIndex] = elements[ elemIndex];
+        elements = bigger;
      }
- }
+	 
+	 
+    /**
+      accessor
+      @return element @index from this list
+      precondition: @index is within the bounds of the elements.
+          (Having warned the user about this precondition,
+           you should NOT complicate your code to check
+           whether user violated the condition.)
+     */
+    public int get( int index ) {
+		return elements[index];
+    }
+
+
+    /**
+      Set value at @index to @newValue
+
+      @return old value at @index
+      @precondition: @index is within the bounds of this list.
+     */
+    public int set( int index, int newValue ) {
+		int previousV = elements[index];
+		elements[index] = newValue;
+		return previousV;
+    }
+
+
+    /**
+      Insert @value at position @index in this list.
+
+      Shift the element currently at that position (if any)
+      and any subsequent elements to the right
+      (that is, increase the index associated with each).
+     */
+     public void add( int index, int value) {
+		int newV = value;
+		int previousV;
+		if (filledElements == elements.length)
+			expand();
+		for (int i = index; i < size(); i++) {
+			previousV = elements[i];
+			elements[i] = newV;
+			newV = previousV;
+		}
+		add(newV);
+    }
+     
+
+
+    /**
+      Remove the element at position @index in this list.
+
+      Shift any subsequent elements to the left (that is,
+      decrease the index associated with each).
+
+      @return the value that was removed from the list
+     */
+     public int remove( int index) {
+		int cycleI;
+		int removedV = elements[index];
+		for (cycleI = index; cycleI < size(); cycleI++) {
+			elements[cycleI] = elements[cycleI + 1];
+		}
+		elements[cycleI] = -1; //arbitrary placeholder value
+		return removedV;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,41 +1,7 @@
-# implement a list by storing its elements in an array
-
-Design `List_inArraySlots` to implement a list of integers.
-Your implementation required to be able to store any values that 
-an `int` can hold, in consecutive slots of an array of `int`s.
-
-In general, the array will be only partially populated with list elements.
-That is, the array is generally larger than this list. 
-Occasionally, the array can be completely full, but adding
-another element to the list will cause the `List_inArraySlots`
-class to expand the list, as described in the header comments
-for the `expand()` method in the skeleton.
-
-The requirement "to store any values that an `int` can hold" 
-implies that the implementation can **not** identify the end of the list
-by the values of the integers stored in the list. That design decision
-would require reserving one integer value as a marker value, which would
-keep the user-programmer from storing that value in the list.
-Therefore, the "marker value" technique is banned for our purposes here.
-(In other contexts,  marker values are fine design choices.)
-###Project Title: List_inArraySlots
-####Members: Sergei Mishchenko, Kyra Gunluk (partner)
-
-####Fields: 
-int filledElements - counts filled elements in array
-int[] list - contains an array of ints
-####Constructors: 
-creates an empty int array and initalizes elementsFIlled variable
-####methods: 
-size() - returns number of filled Elements
-add (int value) - adds value to the end of the list
-expand () - increases size of the list
-addAtHead
-expand 
-
-###Remarks on UserOfList class:
-tests edge and middle casses of methods to ensure they work
-creates an instance of List_inArraySlots.
 # implement a list by storing data in an encapsulated array
 
 ## features to understand
+- Keep **Encapsulated data** `private`.
+- Trace the changes to the heap and stack during
+an invocation of `expand()`.
+
